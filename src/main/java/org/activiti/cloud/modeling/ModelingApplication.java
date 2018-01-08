6d7b94f7a78d47a7a40f10d1e3b361ba402948cc
constantin-ciobotaru <Constantin.Ciobotaru@ness.com>
2018-01-08 18:10:11 +0200

    1628 Create initial modeling application
    - add ModelingApplication
    - add ModelingApplicationTest
    - fix multiple EnableJpaRepositories annotations issue and preapare for docker

---
+++ src/main/java/org/activiti/cloud/modeling/ModelingApplication.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2017 Alfresco, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.activiti.cloud.modeling;
+
+import org.activiti.cloud.org.config.EnableActivitiOrganization;
+import org.activiti.cloud.process.model.EnableActivitiProcessModel;
+import org.activiti.cloud.services.process.model.jpa.version.ExtendedJpaRepositoryFactoryBean;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Modeling application
+ */
+@SpringBootApplication
+//TODO: to remove EnableJpaRepositories and EntityScan from here as they should be in EnableActivitiProcessModel and EnableActivitiOrganization annotation (this is just a tempoarary fix).
+@EnableJpaRepositories(
+        basePackages = {"org.activiti.cloud.services.process.model.jpa", "org.activiti.cloud.services.organization.jpa"},
+        repositoryFactoryBeanClass = ExtendedJpaRepositoryFactoryBean.class)
+@EntityScan({"org.activiti.cloud.services.process.model.core.model", "org.activiti.cloud.organization.core.model"})
+@EnableActivitiProcessModel
+@EnableActivitiOrganization
+@ComponentScan("org.activiti.cloud")
+public class ModelingApplication {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModelingApplication.class,
+                              args);
+    }
+}
