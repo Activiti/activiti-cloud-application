0ede3a8449a6805dfd73a6ffaad414dcc95c7099
Jakub Sokołowski <30300796+jsokolowskii@users.noreply.github.com>
2023-01-04 10:25:56 +0100

    Revert "AAE-11278 fix imports"
    This reverts commit f2872b4a6da56583e6defdcc284f3c8fd82240f3.

--- example-cloud-connector/starter/src/main/java/org/activiti/cloud/examples/connectors/HeadersConnector.java
+++ example-cloud-connector/starter/src/main/java/org/activiti/cloud/examples/connectors/HeadersConnector.java
@@ -17,8 +17,10 @@ package org.activiti.cloud.examples.connectors;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Consumer;
 import org.activiti.cloud.api.process.model.IntegrationRequest;
 import org.activiti.cloud.api.process.model.IntegrationResult;
+import org.activiti.cloud.common.messaging.functional.ConditionalFunctionBinding;
 import org.activiti.cloud.common.messaging.functional.Connector;
 import org.activiti.cloud.common.messaging.functional.ConnectorBinding;
 import org.activiti.cloud.connectors.starter.channels.IntegrationResultSender;
